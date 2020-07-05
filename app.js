bf5cc832affcf6abd7948bb8d7ecda1478756665
ahmadmuhbit <ahmadmuhbit@gmail.com>
2020-07-05 08:40:54 +0700

    Modul NPM (Superhero)

---
+++ app.js
@@ -0,0 +1,10 @@
+// const fs = require("fs");
+
+// fs.copyFileSync("text.txt", "text2.txt" );
+// console.log("text.txt was copied to text.txt");
+
+const hero = require("superheroes");
+
+for(let i = 0; i < 10; i++) {
+    console.log(hero.random());
+}
